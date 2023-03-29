21dc5f6c2752e216c80a8e629ac59940d515c758
MikiKarkowski <mikikarkowski@gmail.com>
2023-03-30 00:23:33 +0200

    Added profile picture

--- PortfolioWebsite/Models/IndexViewModel.cs
+++ PortfolioWebsite/Models/IndexViewModel.cs
@@ -1,33 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System.Drawing;
-using System.Drawing.Imaging;
-
-namespace PortfolioWebsite.Models
+﻿namespace PortfolioWebsite.Models
 {
     public class IndexViewModel
     {
-        public string GetImageSrc() 
-        {
-            string src = @"Images/Test.jpg";
-            bool imageExists = File.Exists(src);
-            return src;
-        }
-
-        //public ActionResult Image()
-        //{
-        //    Bitmap bitmap = new Bitmap(GetImageSrc());
-        //    var bitmapBytes = BitmapToBytes(bitmap); //Convert bitmap into a byte array
-        //    return File(bitmapBytes, "image/jpeg"); //Return as file result
-        //}
-
-        //// This method is for converting bitmap into a byte array
-        //private static byte[] BitmapToBytes(Bitmap img)
-        //{
-        //    using (MemoryStream stream = new MemoryStream())
-        //    {
-        //        img.Save(stream, ImageFormat.Png);
-        //        return stream.ToArray();
-        //    }
-        //}
+        public string ProfilePictureSrc = @"Images/ProfilePicture.jpg";
+        public string TestImageSrc = @"Images/Test.jpg";
+   
     }
 }
