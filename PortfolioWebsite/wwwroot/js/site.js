4072d8c980bc11929c83e40669fbdbac53c2fde6
MikiKarkowski <mikikarkowski@gmail.com>
2023-04-02 19:19:29 +0200

    Added simple animations

--- PortfolioWebsite/wwwroot/js/site.js
+++ PortfolioWebsite/wwwroot/js/site.js
@@ -2,3 +2,18 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).on("scroll", function() {
+  var pageTop = $(document).scrollTop();
+  var pageBottom = pageTop + $(window).height();
+  var tags = $(".tag");
+
+  for (var i = 0; i < tags.length; i++) {
+    var tag = tags[i];
+
+    if ($(tag).position().top < pageBottom) {
+      $(tag).addClass("visible");
+    } else {
+      $(tag).removeClass("visible");
+    }
+  }
+});
