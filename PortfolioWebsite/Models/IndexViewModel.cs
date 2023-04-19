e660db3f4aa49d806f17646bcf459575eda7426e
MikiKarkowski <mikikarkowski@gmail.com>
2023-04-19 19:45:41 +0200

    Added cover photo to main page

--- PortfolioWebsite/Models/IndexViewModel.cs
+++ PortfolioWebsite/Models/IndexViewModel.cs
@@ -4,6 +4,7 @@
     {
         public string ProfilePictureSrc = @"Images/ProfilePicture.png";
         public string ExercisesSrc = @"Images/Exercises.jpg";
+        public string CoverPhotoSrc = @"Images/CoverPhoto.jpg";
         public string RumikAppSrc = @"Images/RumikApp.jpg";
         public string ViToolsSrc = @"Images/ViTools.jpg";
     }
